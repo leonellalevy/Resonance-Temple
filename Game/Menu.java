68b7c90ed846a1d58eb6f8ae1e4626eb08d3d4ab
Leonella Levy Martel <leonellalevymartel@gmail.com>
2021-12-02 18:38:58 -0500

    Add sound to menu

--- Game/Menu.java
+++ Game/Menu.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Menu extends World
 {
+    public static GreenfootSound music = new GreenfootSound("RealBobChanson.wav");
     /**
      * Constructor for objects of class MenuPage.
      * 
@@ -24,6 +25,7 @@ public class Menu extends World
 
     public void act()
     {
+        music.playLoop();
         MouseInfo mouse = Greenfoot.getMouseInfo();
         if (mouse != null) 
         {
